28906933599c652bf8b87ceb7875866458f973f4
Parate <parag.parate@accenture.com>
2022-04-28 12:00:26 -0400

    [init] added spring-boot project

---
+++ src/test/java/com/axeman/auth/service/UserAccountServiceTest.java
@@ -0,0 +1,17 @@
+package com.axeman.auth.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class UserAccountServiceTest {
+
+    @BeforeEach
+    public void setup() {
+
+    }
+
+    @Test
+    public void shouldCreateNewAccount() {
+
+    }
+}
