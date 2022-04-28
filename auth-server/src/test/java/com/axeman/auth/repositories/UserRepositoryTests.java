0d16933dab218e66fa1d722f018c3c312a83401f
Parate <parag.parate@accenture.com>
2022-04-28 19:36:17 -0400

    [feat] added commons module with spring-security for hashing support
    Added new module to support shared components. Added spring security
    dependency to commons module to support password hashing.

--- auth-server/src/test/java/com/axeman/auth/repositories/UserRepositoryTests.java
+++ auth-server/src/test/java/com/axeman/auth/repositories/UserRepositoryTests.java
@@ -1,15 +1,37 @@
 package com.axeman.auth.repositories;
 
+import com.axeman.auth.entities.User;
+import com.axeman.auth.models.UserAccountStatus;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.test.context.ActiveProfiles;
 
 @DataJpaTest
+@ActiveProfiles("test")
 class UserRepositoryTests {
 
     @Autowired
     private UserRepository userRepository;
 
+    User testUser;
+
+    @BeforeEach
+    void setup() {
+        testUser = User.builder()
+                .username("test-user")
+                .password("test-password")
+                .status(UserAccountStatus.ACTIVE)
+                .primaryEmail("test-email@gmail.com")
+                .build();
+    }
+
+    @Test
+    void shouldCreateNewUserAccount() {
+        User newUser = userRepository.save(testUser);
+        assertEquals("test-user", newUser.getUsername());
+    }
 
 }
